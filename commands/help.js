d8fa2ebf3c3b72df1cfd068ce5542250e879847e
SMOOTH_MC <94683168+SMOOTH-MC@users.noreply.github.com>
2022-07-07 13:51:16 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+//THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
+module.exports = {
+    name: 'help',
+    description: "To get bot commands",
+    run(client, message, args , Discord){
+      const Embed = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setTitle('MINNAL BOT COMMANDS')
+      .setFooter('CREDITS TO TEAM MINNAL')
+      .addFields(
+        {name: 'INFO', value: '``` help , ping , users ```'},
+        {name: 'MODRATION', value: '``` purge , kick ```'}
+      )
+      message.channel.send(Embed);
+    }
+}
+//THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
