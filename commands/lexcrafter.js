225b74a97e1119194557c2ddb9414e1a644313e9
SMOOTH_MC <94683168+SMOOTH-MC@users.noreply.github.com>
2022-07-07 22:49:45 +0530

    Update lexcrafter.js

--- commands/lexcrafter.js
+++ commands/lexcrafter.js
@@ -1,7 +1,7 @@
 //THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
 module.exports = {
-    name: 'ping',
-    description: "Use To Know Bot Ping",
+    name: 'lexcrafter',
+    description: "about lexcrafter",
     run(client, message, args , Discord){
       const Embed = new Discord.MessageEmbed()
       .setColor('RANDOM')
@@ -12,6 +12,7 @@ module.exports = {
         {name: 'About', value: 'LEX CRAFTER IS THE GREATEST KILLADI IN MINNAL CRAFT'},
         {name: 'Hobby', value: 'LEX CRAFTER LOVES TO PLAY BEDWARS & BGMI LOVES TO WATCH ANIME AND ALSO WILL BE THERE ON VC EVERYTIME'},
         {name: 'Real Name', value: 'UNDEFINED'},
+        {name: 'Relation', value: 'Mingle'},
         {name: 'FOLLOW ', value: '[Youtube](https://www.youtube.com/watch?v=dQw4w9WgXcQ) , [Instagtam](https://www.instagram.com/lex_crafter)'}
       )
       message.channel.send(Embed);
