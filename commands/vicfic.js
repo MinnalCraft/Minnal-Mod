c78a326d33fca04f70ea9e0011235990b568410b
SMOOTH_MC <94683168+SMOOTH-MC@users.noreply.github.com>
2022-07-07 22:50:56 +0530

    Update vicfic.js

--- commands/vicfic.js
+++ commands/vicfic.js
@@ -1,7 +1,7 @@
 //THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
 module.exports = {
-    name: 'ping',
-    description: "Use To Know Bot Ping",
+    name: 'vicfic',
+    description: "about vicfic",
     run(client, message, args , Discord){
       const Embed = new Discord.MessageEmbed()
       .setColor('RANDOM')
@@ -12,6 +12,7 @@ module.exports = {
         {name: 'About', value: 'VICFIC IS A PYTHON EXPERT WHO CODED MINNUZZ'},
         {name: 'Hobby', value: 'VICFIC LIVES TO CODE MAILY PYTHON AND TO PLAY VALORANT'},
         {name: 'Real Name', value: 'UNDEFINED'},
+        {name: 'Relation', value: 'Single'},
         {name: 'FOLLOW ', value: '[Youtube](https://www.youtube.com/channel/UCf8H1ar74cpo04O0mENRgqQ)'}
       )
       message.channel.send(Embed);
