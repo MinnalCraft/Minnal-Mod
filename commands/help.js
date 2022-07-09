7b959004fdde08a1f3daf4be4ef5cbaefd3738bc
SMOOTH_MC <94683168+SMOOTH-MC@users.noreply.github.com>
2022-07-09 10:21:03 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,12 @@ module.exports = {
       const Embed = new Discord.MessageEmbed()
       .setColor('RANDOM')
       .setTitle('MINNAL BOT COMMANDS')
-      .setFooter('CREDITS TO TEAM MINNAL')
+      .setDescription('_BY TEAM_ **MINNAL**')
+      .setFooter('MINNAL CRAFT OFFICIAL BOT')
       .addFields(
         {name: 'INFO', value: '``` help , ping , users , botinfo ```'},
-        {name: 'MODRATION', value: '``` purge , kick , mute , unmute , addrole , removerole ```'}
+        {name: 'MODRATION', value: '``` purge , kick , mute , unmute , addrole , removerole ```'},
+        {name: 'Links', value: '[Website](https://minnalcraft.ml) | [Discord](https://dsc.gg/minnalcraft)'}
       )
       message.channel.send(Embed);
     }
