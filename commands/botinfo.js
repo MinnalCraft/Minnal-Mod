9f830a367b0bd91547d8c19c7c411b83d31196fe
SMOOTH_MC <94683168+SMOOTH-MC@users.noreply.github.com>
2022-07-09 09:41:47 +0530

    Create botinfo.js

---
+++ commands/botinfo.js
@@ -0,0 +1,21 @@
+//THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
+module.exports = {
+    name: 'smooth',
+    description: "about smooth",
+    run(client, message, args , Discord){
+      const Embed = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setTitle('ABOUT ME')
+      .setDescription('MY NAME IS MINNAL MOD')
+      .setFooter('CREDITS TO TEAM MINNAL')
+      .addFields(
+        {name: 'Developed By', value: 'TEAM MINNAL'},
+        {name: 'Ping', value: `${client.ws.ping} ms`},
+        {name: 'Version', value: `${require("../package.json").version}`},
+        {name: 'Servers', value: `${client.guilds.cache.size}`},
+        {name: 'Node.js', value: `${process.version}`}
+      )
+      message.channel.send(Embed);
+    }
+}
+//THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
