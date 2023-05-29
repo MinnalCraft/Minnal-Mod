882baa0753c2ca3d9283cca4422ab9d4edf3f9ce
MinnalCraft <104630044+MinnalCraft@users.noreply.github.com>
2023-05-29 20:48:45 +0530

    Update and rename users.js to crew.js

--- commands/crew.js
+++ commands/crew.js
@@ -1,15 +1,15 @@
 //THIS BOT IS NOT A CLONE ITS 100% CODED BY TEAM MINNAL
 module.exports = {
-    name: 'ping',
-    description: "Use To Know Bot Ping",
+    name: 'crew',
+    description: "Use To Know about TVD CREW",
     run(client, message, args , Discord){
       const Embed = new Discord.MessageEmbed()
       .setColor('RANDOM')
-      .setTitle('USERS')
+      .setTitle('TVD CREW')
       .setDescription('`~` **+** `USERNAME`')
       .setFooter('THARAVAD NETWORK')
       .addFields(
-        {name: 'USERS', value: '``` mallukid , lexmwon , smooth , dinkan , koku , crogo , stormgirl , vicfic ```'},
+        {name: 'CREWMATES', value: '``` mallukid , lexmwon , smooth , dinkan , koku , crogo , stormgirl , vicfic ```'},
         {name: 'Links', value: '[Website](https://tharavad.net) | [Discord](https://discord.gg/yzrfnMubNW)'}
       )
       message.channel.send(Embed);
